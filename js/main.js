b3f810f89bdb4a8a76b63b47b8a73bcec006ac10
Avneesh Kumar <avneesh.kumar@gmail.com>
2015-01-02 07:02:16 -0800

    update

--- js/main.js
+++ js/main.js
@@ -6,17 +6,21 @@ var app = {
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
             // ... if yes: register touch event listener to change the "selected" state of the item
             $('body').on('touchstart', 'a', function(event) {
+                alert('touchstart');
                 $(event.target).addClass('tappable-active');
             });
             $('body').on('touchend', 'a', function(event) {
+                alert('touchend');
                 $(event.target).removeClass('tappable-active');
             });
         } else {
             // ... if not: register mouse events instead
             $('body').on('mousedown', 'a', function(event) {
+                 alert('mousedown');
                 $(event.target).addClass('tappable-active');
             });
             $('body').on('mouseup', 'a', function(event) {
+                alert('mouseup');
                 $(event.target).removeClass('tappable-active');
             });
         }
