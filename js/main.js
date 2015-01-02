df08806ff6a20e46822fb2ceef3bd8a5617019a7
Avneesh Kumar <avneesh.kumar@gmail.com>
2015-01-02 08:05:08 -0800

    update

--- js/main.js
+++ js/main.js
@@ -2,34 +2,16 @@ var app = {
    registerEvents: function() {
         var self = this;
         // Check of browser supports touch events...
-        //alert(document.documentElement.hasOwnProperty('ontouchstart'));
+          
 
-        if ("ontouchstart" in document.documentElement)
-        {
-          alert('iphone');
-        }
-        else
-        {
-         alert('pc');
-        } 
-
-           // $('body').on('touchstart', 'a', function(event) {
-           //      $(event.target).addClass('tappable-active');
-           //  });
-           //  $('body').on('touchend', 'a', function(event) {
-           //      $(event.target).removeClass('tappable-active');
-           //  });
-
-        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        if ("ontouchstart" in document.documentElement){
             // ... if yes: register touch event listener to change the "selected" state of the item
-            // $('body').on('touchstart', 'a', function(event) {
-            //     alert('touchstart');
-            //     $(event.target).addClass('tappable-active');
-            // });
-            // $('body').on('touchend', 'a', function(event) {
-            //     alert('touchend');
-            //     $(event.target).removeClass('tappable-active');
-            // });
+            $('body').on('touchstart', 'a', function(event) { 
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) { 
+                $(event.target).removeClass('tappable-active');
+            });
         } else {
             // ... if not: register mouse events instead
             $('body').on('mousedown', 'a', function(event) {
